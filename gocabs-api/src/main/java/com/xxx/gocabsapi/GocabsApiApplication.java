44f1432695f88bc47abf13c8bbf7995147d2295d
K.Young <154226491@qq.com>
2024-07-19 18:42:10 +0800

    初始化

---
+++ gocabs-api/src/main/java/com/xxx/gocabsapi/GocabsApiApplication.java
@@ -0,0 +1,13 @@
+package com.xxx.gocabsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GocabsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GocabsApiApplication.class, args);
+	}
+
+}
